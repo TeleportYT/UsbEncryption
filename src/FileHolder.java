55626cc92569afbbd5c3c25a5c8bd80720e08f08
Vivien Evg <vivien110705@gmail.com>
2024-03-07 11:36:08 +0200

    Encrypting and decrypting is very fast, zip files work, binary files work now need to go over each usb file and encrypt it

--- src/FileHolder.java
+++ src/FileHolder.java
@@ -6,59 +6,80 @@ import Model.ThreadBlock;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class FileHolder {
     private String filepath;
     private Queue<Block> blocks;
+
+    private Boolean mode;//True-Encrypt False-Decrypt
     private static final int CHUNK_SIZE = 1024 * 1024; // 1 MB
 
     public FileHolder(String filepath) {
         this.filepath = filepath;
-        this.blocks = new ArrayDeque<>();
-        readAndSeparateIntoBlocks();
+        this.blocks = readFileIntoBlocks(filepath);
     }
 
-    private void readAndSeparateIntoBlocks() {
-        try (FileInputStream fileInputStream = new FileInputStream(filepath);
-             FileChannel fileChannel = fileInputStream.getChannel()) {
-            long fileSize = fileChannel.size();
-            long position = 0;
+    private Queue<Block> readFileIntoBlocks(String filepath) {
+        Queue<Block> blocks = new LinkedList<>();
 
-            while (position < fileSize) {
-                ByteBuffer byteBuffer = ByteBuffer.allocate(CHUNK_SIZE);
-                int bytesRead = fileChannel.read(byteBuffer, position);
-                position += bytesRead;
+        try (InputStream inputStream = new FileInputStream(filepath)) {
+            if (filepath.endsWith(".zip")) {
+                ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+                ZipEntry zipEntry;
 
-                if (bytesRead == CHUNK_SIZE || position == fileSize) {
-                    byteBuffer.flip();
-                    byte[] buffer = new byte[bytesRead];
-                    byteBuffer.get(buffer);
-                    blocks.add(new Block(bytesToHexString(buffer)));
-                } else {
-                    byteBuffer.flip();
-                    byte[] buffer = new byte[bytesRead];
-                    byteBuffer.get(buffer);
-                    blocks.add(new Block(bytesToHexString(buffer)));
-                    position -= (CHUNK_SIZE - bytesRead);
+                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                    byte[] buffer = new byte[16]; // read 16 bytes at a time
+                    int bytesRead;
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                    while ((bytesRead = zipInputStream.read(buffer)) != -1) {
+                        byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    }
+
+                    String hexString = bytesToHexString(byteArrayOutputStream.toByteArray(),16);
+                    blocks.add(new Block(hexString));
+                }
+            } else {
+                byte[] buffer = new byte[16]; // read 16 bytes at a time
+                int bytesRead;
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    String hexString = bytesToHexString(buffer, bytesRead);
+                    blocks.add(new Block(hexString));
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return blocks;
     }
 
-    private String bytesToHexString(byte[] bytes) {
-        StringBuilder hexNumbers = new StringBuilder();
-        for (byte b : bytes) {
-            hexNumbers.append(String.format("%02x", b));
+
+    private String bytesToHexString(byte[] bytes, int length) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(String.format("%02x", bytes[i]));
         }
-        return hexNumbers.toString();
+
+        return sb.toString();
     }
 
     public String getFilePath() {
@@ -69,23 +90,111 @@ public class FileHolder {
         return blocks;
     }
 
-    public void writeBlocksToBinaryFile(String outputFilePath) {
-        try (RandomAccessFile randomAccessFile = new RandomAccessFile(outputFilePath, "rw");
-             FileChannel fileChannel = randomAccessFile.getChannel()) {
-            long position = 0;
-
-            for (Block block : blocks) {
-                String hexString = AES.writeMatrix(block.getData(),4,4);
-                byte[] binaryData = hexStringToBytes(hexString);
-
-                ByteBuffer byteBuffer = ByteBuffer.wrap(binaryData);
-                fileChannel.write(byteBuffer, position);
-                position += binaryData.length;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void writeBlocksToFile(String outputFilePath) throws IOException {
+        if (outputFilePath.endsWith(".zip")) {
+            writeBlocksToZipFileWithEncryptedFiles(outputFilePath);
+        } else {
+            writeBlocksToNonZipFile(outputFilePath);
         }
     }
+
+    private void writeBlocksToZipFileWithEncryptedFiles(String outputFilePath) throws IOException {
+        Path tempDirectory = Files.createTempDirectory("temp");
+        Path zipFilePath = Paths.get(outputFilePath);
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(filepath))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                Path extractedFilePath = tempDirectory.resolve(zipEntry.getName());
+                Files.createDirectories(extractedFilePath.getParent());
+                Files.copy(zipInputStream, extractedFilePath);
+
+                Key k = new Key(KeyGenerator.generateKey("vladi1977", "kjhjkhjlkjlkj").toCharArray());
+                AES algo = new AES(k);
+
+                FileHolder fileHolder = new FileHolder(extractedFilePath.toString());
+                ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+                for (Block block : fileHolder.blocks) {
+                    ThreadBlock bl = new ThreadBlock(block, algo, Boolean.FALSE);
+                    executorService.submit(bl);
+                }
+                executorService.shutdown();
+                executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+                extractedFilePath.toFile().delete();
+                Files.copy(fileHolder.getBlocksAsInputStream(), tempDirectory.resolve(zipEntry.getName()));
+                System.out.println("Next file");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFilePath.toFile()))) {
+            for (File file : tempDirectory.toFile().listFiles()) {
+                addFileToZip(zipOutputStream, file, file.getName());
+                System.out.println("Next Add");
+            }
+        }
+        System.out.println("Next Finish");
+        Files.walk(tempDirectory).map(Path::toFile).forEach(File::delete);
+        System.out.println("End");
+    }
+
+    private void addFileToZip(ZipOutputStream zipOutputStream, File file, String entryName) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            ZipEntry zipEntry = new ZipEntry(entryName);
+            zipOutputStream.putNextEntry(zipEntry);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                zipOutputStream.write(buffer, 0, bytesRead);
+            }
+            zipOutputStream.closeEntry();
+        }
+    }
+
+    private InputStream getBlocksAsInputStream() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        for (Block block : blocks) {
+            byte[] data = hexStringToBytes(AES.writeMatrix(block.getData(), 4, 4));
+            outputStream.write(data);
+        }
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+
+    private void writeBlocksToNonZipFile(String outputFilePath) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(outputFilePath)) {
+            int numBlocks = blocks.size();
+            byte[] blockData = new byte[numBlocks * 16]; // Each block is 16 bytes long
+            int offset = 0;
+            for (Block block : blocks) {
+                byte[] data = hexStringToBytes(AES.writeMatrix(block.getData(), 4, 4));
+                System.arraycopy(data, 0, blockData, offset, data.length);
+                offset += data.length;
+
+                fileOutputStream.write(blockData, 0, data.length);
+            }
+        }
+    }
+
+    private String getZipEntryName(String outputFilePath, Block block) {
+        Queue<Block> blocksQueue = new LinkedList<>();
+        blocksQueue.add(block);
+
+        String zipEntryName = outputFilePath;
+        int lastSlashIndex = zipEntryName.lastIndexOf('\\');
+        if (lastSlashIndex != -1) {
+            zipEntryName = zipEntryName.substring(lastSlashIndex + 1);
+        }
+
+        while (!blocksQueue.isEmpty()) {
+            Block currentBlock = blocksQueue.poll();
+            zipEntryName = zipEntryName.concat("_" + currentBlock.getData());
+        }
+
+        return zipEntryName;
+    }
+
     private byte[] hexStringToBytes(String hexString) {
         int len = hexString.length();
         byte[] data = new byte[len / 2];
@@ -98,19 +207,9 @@ public class FileHolder {
         return data;
     }
 
-    public void EnctyptFile(AES aes) {
-        for (Block block : blocks) {
-            aes.Encrypt(block);
-        }
-    }
 
-    public void DecryptFile(AES aes) {
-        for (Block block : blocks) {
-            aes.Decrypt(block);
-        }
-    }
     public static void main(String[] args) throws Exception {
-        FileHolder fileHolder = new FileHolder("C:\\Users\\Vivien\\Downloads\\17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso");
+        FileHolder fileHolder = new FileHolder("C:\\Users\\Vivien\\Downloads\\snowy-mountain-peak-starry-galaxy-majesty-generative-ai.zip");
         System.out.println("File Path: " + fileHolder.getFilePath());
 
         Key k = new Key(KeyGenerator.generateKey("vladi1977", "kjhjkhjlkjlkj").toCharArray());
@@ -119,13 +218,14 @@ public class FileHolder {
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         for (Block block : fileHolder.blocks) {
-            ThreadBlock bl = new ThreadBlock(block, algo, Boolean.TRUE);
+            ThreadBlock bl = new ThreadBlock(block, algo, Boolean.FALSE);
             executorService.submit(bl);
         }
 
         executorService.shutdown();
         executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
-        fileHolder.writeBlocksToBinaryFile("C:\\Users\\Vivien\\Downloads\\webrootE.zip");
+        System.out.println("Saving file");
+        fileHolder.writeBlocksToFile("C:\\Users\\Vivien\\Downloads\\snowy-mountain-peak-starry-galaxy-majesty-generative-ai.zip");
+        System.out.println("Finished");
     }
 }
